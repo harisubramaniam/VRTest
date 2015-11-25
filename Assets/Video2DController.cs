87f2a82654f522d3c4369bac1ae4d1d5d5c8b376
j_shannon_p <johnshannonperkins@gmail.com>
2015-11-24 20:10:49 -0800

    added 2D video viewer

---
+++ Assets/Video2DController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Video2DController : MonoBehaviour {
+
+	public MediaPlayerCtrl srcMediaPlayer;
+	public string srcMedia;
+
+	protected bool isRunContent = false;
+
+	void Start () {
+		srcMediaPlayer.UnLoad();
+	}
+
+	void Update () {
+		if (isRunContent) {
+			srcMediaPlayer.Play ();
+		} else {
+			srcMediaPlayer.Stop ();
+		}
+	}
+
+	public void DoToggleVideo(){
+		if (isRunContent) {
+			isRunContent = false;
+			srcMediaPlayer.UnLoad();
+		} else if (!isRunContent) {
+			isRunContent = true;
+			srcMediaPlayer.Load(srcMedia);
+		}
+	}
+}
